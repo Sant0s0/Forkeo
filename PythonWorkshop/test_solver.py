aae6c0848a5958d9e008fd5775269ca2df232740
Sant0s0 <SantiWorldYT@gmail.com>
2024-05-17 07:49:36 -0500

    cambios

--- PythonWorkshop/test_solver.py
+++ PythonWorkshop/test_solver.py
@@ -9,11 +9,7 @@ class MyTestCase(unittest.TestCase):
         s = Solver()
 
         self.assertRaises(Exception)
-
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
+        
 if __name__ == '__main__':
     unittest.main()
 
